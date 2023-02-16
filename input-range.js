63d7cbebb727fba31798e97ffc5f970aeb1bd372
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-02-16 19:34:16 +0100

    add style and script crossbrowser similar looking input range

--- input-range.js
+++ input-range.js
@@ -1,15 +1,31 @@
 module.exports = function(app){
     var InputRange = Object.getPrototypeOf(app).InputRange = new app.Component("input-range");
     // InputRange.debug = true;
-    InputRange.createdAt      = "2.0.1";
-    InputRange.lastUpdate     = "2.0.1";
+    InputRange.createdAt      = "2.0.2";
+    InputRange.lastUpdate     = "2.0.2";
     InputRange.version        = "1";
     // InputRange.factoryExclude = true;
     // InputRange.loadingMsg     = "This message will display in the console when component will be loaded.";
     // InputRange.requires       = [];
+    InputRange.prototype.onCreate = function(){
+        var range = this;
+        range.min = (range.min !== undefined) ? range.min : +range.getAttr('min', 0);
+        range.max = (range.max !== undefined) ? range.max : +range.getAttr('max', 100);
+        range.val = (range.val !== undefined) ? range.val : +range.getAttr('value', 50);
 
-    // InputRange.prototype.onCreate = function(){
-    // do thing after element's creation
-    // }
+        range.$el.attr('style','--min: '+range.min+'; --max: '+range.max+'; --val:'+range.val+'');
+        range.$el.on('input', function(){
+            this.style.setProperty('--val', +this.value)
+        }).trigger('input');
+
+        return range;
+    }
+    
+    $(function () {
+        $('input[type="range"]').not('.custom')['input-range']();
+        utils.addHtmlHook('input[type="range"]:not(.custom)', function(item){
+            item['input-range']();
+        });
+    });
     return InputRange;
 }
